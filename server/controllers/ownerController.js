3fb3e778981cb22c991319d4b76b7cd09e342130
Saishankar-7 <tumpalasaisankar@gmail.com>
2026-01-03 14:29:28 +0530

    updated the imort error

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,4 +1,4 @@
-import imagekit from "../config/imagekit.js";
+import imagekit from "../config/imageKit.js";
 import User from "../models/User.js";
 import fs from "fs"
 import Car from "../models/car.js";
